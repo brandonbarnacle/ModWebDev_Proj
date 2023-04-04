1120971839484904773852c8f5394797b9b17e87
Mark Barrett <pbarret2@nd.edu>
2023-04-04 15:42:53 -0400

    Cleanup

--- src/Common/ProtectedRoute.js
+++ src/Common/ProtectedRoute.js
@@ -1,14 +1,9 @@
 import React, { useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { checkUser } from "../Components/Auth/AuthService";
 
 const ProtectedRoute = ({ element: Component, ...rest }) => {
     const navigate = useNavigate();
-
-    // Give the user an option to go to log in screen if they are unauthorized to view resource
-    const goBackHandler = () => {
-        navigate('/auth/login');
-    };
 
     useEffect(() => {
         // if the user is authorized...
@@ -22,19 +17,6 @@ const ProtectedRoute = ({ element: Component, ...rest }) => {
             navigate("/auth/login");
         }
       }, []);
-
-    // // if the user is authorized...
-    // if (checkUser()) {
-    //     return <Component />;
-    // // if the user is unauthorized...
-    // } else {
-    //     // return (
-    //     //     <div>
-    //     //         <p>Unauthorized!</p> <button onClick={goBackHandler}>Log in.</button>
-    //     //     </div>
-    //     // );
-    //     navigate('/auth/login');
-    // }
 };
 
 export default ProtectedRoute;
